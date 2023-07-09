eba55f672f531734b21ef7f45abf0e0d2572d6e4
bhttchr6 <47575638+bhttchr6@users.noreply.github.com>
2023-07-09 08:01:11 -0700

    Update client.js

--- js/client.js
+++ js/client.js
@@ -10,7 +10,7 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-scene.background = new THREE.Color( 0xffffff );
+//scene.background = new THREE.Color( 0xffffff );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
